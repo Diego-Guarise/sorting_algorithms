b7bae1bf8de1f4ceef930a523e60c4de4c6ecb9a
unknown <l.guariselopez@gmail.com>
2021-10-25 11:35:52 -0300

    fix code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,18 +8,22 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int tmp;
+	size_t i, tmp, x;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || size == 1)
+		return;
+
+	for (i = 1; i < size - 1; i++)
 	{
-		if (array[i] > array[i + 1])
-		{
-			tmp = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = tmp;
-			print_array(array, size);
-			bubble_sort(array, size);
+		for (x = 0; i < size - 1; x++)
+		{	
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
 		}
 	}
 }
