a3eaa76127c285f0d546578c3e2df66e8ff46e02
unknown <l.guariselopez@gmail.com>
2021-10-25 11:51:51 -0300

    code fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,12 +10,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, tmp, x;
 
-	if (array == NULL || size == 1)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 1; i < size; i++)
 	{
-		for (x = 0; i < size - 1; x++)
+		for (x = 0; x < size - 1; x++)
 		{
 			if (array[i] > array[i + 1])
 			{
