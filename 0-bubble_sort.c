d55ab52305ad9bc9d8dd7496fc41e1005d3fdccd
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-25 07:09:48 -0700

    task complete

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 /**
-  * bubble_sort - Write a function that sorts an array of integers in ascending order using the Bubble sort algorithm
+  * bubble_sort - Write a function that sorts an array of
+  * integers in ascending order using the Bubble sort algorithm
   * @array: array int
   * @size: size_t
   */
@@ -14,11 +15,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		if (array[i] > array[i + 1])
 		{
-			tmp = array[i + 1];
-			array[i + 1] = array[i];
-			array[i] = tmp;
-            i = -1;
+			tmp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = tmp;
+			print_array(array, size);
+			bubble_sort(array, size);
 		}
-        print_array(array, size);
 	}
 }
