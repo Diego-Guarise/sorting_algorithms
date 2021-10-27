1c120a6aa46abbf52d04fa69a535136fa87cc2cb
unknown <l.guariselopez@gmail.com>
2021-10-26 21:40:54 -0300

    not end

---
+++ 3-quick_sort.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ *part_array - partition array
+ *@lo: change 1
+ *@hi: chagne 2
+ *@array: random number array
+ *@size: array size
+ *Return: the value of the pivot
+ */
+int part_array(int *array, int lo, int hi, size_t size)
+{
+    if (array || lo || hi || size)
+    {
+        return;
+    }
+    
+}
