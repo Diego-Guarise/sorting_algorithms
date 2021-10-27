88077a77ec0831b8196bb5330a1ad43b6321de4e
unknown <l.guariselopez@gmail.com>
2021-10-26 21:52:33 -0300

    not end

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * quick_sort_hoare - reparte cartas, lo siento ro no lo hice.
+ * @array: direccion puntero
+ * @size: tamaño array
+ */
+
+void quick_sort_hoare(int *array, size_t size);
+{
+    if (array || size)
+    {
+        return;
+    }
+}
