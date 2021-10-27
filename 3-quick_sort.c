3a8e24a200b55f8226900c4f02f44edf78b61694
unknown <l.guariselopez@gmail.com>
2021-10-26 22:03:33 -0300

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,7 @@
 #include "sort.h"
 
 /**
- *part_array - partition array
- *@lo: change 1
- *@hi: chagne 2
+ *quick_sort - partition array
  *@array: random number array
  *@size: array size
  *Return: the value of the pivot
